4e9c231967edfbc655519dd6c4cc9efedb812447
Shuby Mishra <shubymishra20@gmail.com>
2023-02-24 15:36:20 -0500

    more general auth tests for framework

--- hyprxa/tests/test_auth.py
+++ hyprxa/tests/test_auth.py
@@ -19,23 +19,21 @@ from starlette.requests import HTTPConnection, Request
 from starlette.authentication import AuthCredentials, AuthenticationError
 from starlette.middleware.authentication import AuthenticationMiddleware
 
-from fastapi import FastAPI, APIRouter, Depends
+from fastapi import FastAPI, APIRouter, HTTPException, Depends
 
 class BasicAuthClient(AuthenticationClient):
     "Basic authentication client with hardcoded credentials."
-    def __init__(self, authenticated_users: Set , unauthenticated_users: Set):
+    def __init__(self, authenticated_users):
         self.authenticated_users = authenticated_users
-        self.unauthenticated_users = unauthenticated_users
 
     def authenticate(self, username: str, password: str) -> bool:
         return username in self.authenticated_users
 
     def get_user(self, username: str) -> BaseUser:
         if username in self.authenticated_users:
-            return BaseUser(username=username, scopes={"authenticated", "ADMIN"})
-        elif username in self.unauthenticated_users:
-            return BaseUser(username=username, scopes={"unauthenticated"})
-        return UserNotFound(username)
+            scopes = {"authenticated"} if username != "admin" else {"authenticated", "ADMIN"}
+            return BaseUser(username=username, scopes=scopes)
+        raise UserNotFound(username)
     
 class BasicAuth(BaseAuthenticationBackend): 
     async def authenticate(self, conn: HTTPConnection) -> Tuple[AuthCredentials, BaseUser] | None:
@@ -43,7 +41,8 @@ class BasicAuth(BaseAuthenticationBackend):
             return None
 
         auth = conn.headers.get("Authorization")
-        if auth is None: return 
+        if auth is None: 
+            return 
 
         scheme, credentials = auth.split()
 
@@ -52,9 +51,8 @@ class BasicAuth(BaseAuthenticationBackend):
 
         
         username = self.handler.validate(credentials)
-
-
-        if username is None: return
+        if username is None: 
+            return
 
         try:
             user = self.client.get_user(username)
@@ -72,7 +70,7 @@ def token_handler() -> TokenHandler:
 def app(token_handler) -> FastAPI:
     app = FastAPI(title="Test Authentication")
 
-    auth_client = BasicAuthClient({"admin"}, {"anonymous"})
+    auth_client = BasicAuthClient({"admin", "user"})
 
     auth_backend = BasicAuth(client=auth_client, handler=token_handler)
 
@@ -113,11 +111,37 @@ def test_valid_headers(test_client: TestClient, token_handler: TokenHandler):
     assert response.status_code == 200
     assert response.content == b'true'
 
-def test_invalid_username(test_client: TestClient, token_handler: TokenHandler):
+def test_invalid_user(test_client: TestClient, token_handler: TokenHandler):
     token = token_handler.issue(claims={"sub": "non_user"})
     response = test_client.get("/whoami", headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == 500
+    assert response.json().get("detail") == "The request cannot be completed. non_user not found."
 
+def test_valid_user(test_client: TestClient, token_handler: TokenHandler):
+    token = token_handler.issue(claims={"sub": "admin"})
+    response = test_client.get("/whoami", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+    assert response.json() ==  {'username': 'admin', 'scopes': ['ADMIN', 'authenticated']}
+
+
+def test_requires_scopes(test_client: TestClient, token_handler: TokenHandler):
+    admin_token = token_handler.issue(claims={"sub": "admin"})
+    response = test_client.get("/requires_admin_scope", headers={"Authorization": f"Bearer {admin_token}"})
+
+    assert response.status_code == 200
+
+    user_token = token_handler.issue(claims={"sub": "user"})
+    response = test_client.get("/requires_admin_scope", headers={"Authorization": f"Bearer {user_token}"})
+
+    assert response.status_code == 403
+
+    response = test_client.get("/requires_admin_scope", headers=None) # no headers
+    assert response.status_code == 401
+
+
+
+
+    
 
 
 
